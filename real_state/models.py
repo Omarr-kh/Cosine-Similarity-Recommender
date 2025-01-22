4f7dcb6dc0502bd0b246ba38bd8fcf4f625c3fa8
Omar <omar1752003@gmail.com>
2025-01-22 09:47:32 +0200

    created RealState model

--- real_state/models.py
+++ real_state/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class RealState(models.Model):
+    price = models.PositiveBigIntegerField()
+    bedrooms = models.PositiveIntegerField()
+    bathrooms = models.PositiveIntegerField()
+    sqft = models.DecimalField(max_digits=10, decimal_places=3)
+    year_built = models.PositiveIntegerField()
+    property_type = models.CharField(
+        max_length=50,
+        choices=[('residential', 'Residential'), ('commercial', 'Commercial')],
+        default='residential',
+    )
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    parking_spaces = models.PositiveIntegerField(default=0)
+    has_garage = models.BooleanField(default=False)
+    has_pool = models.BooleanField(default=False)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Real Estate Property"
+        verbose_name_plural = "Real Estate Properties"
