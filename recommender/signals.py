fb2de950b0467ed04c1fe3387ed703f5b0aa629b
Omar <omar1752003@gmail.com>
2025-01-22 12:55:06 +0200

    updates recommender's data on database updates

--- recommender/signals.py
+++ recommender/signals.py
@@ -6,10 +6,29 @@ from .system import real_state_recommender
 
 
 @receiver(post_save, sender=RealState)
-def update_recommendation_on_save(sender, instance, **kwargs):
-    real_state_recommender.load_properties()
+def handle_property_save(sender, instance, created, **kwargs):
+    # Create the property data from the instance
+    property_data = {
+        'id': instance.id,
+        'price': instance.price,
+        'bedrooms': instance.bedrooms,
+        'bathrooms': instance.bathrooms,
+        'sqft': instance.sqft,
+        'year_built': instance.year_built,
+        'parking_spaces': instance.parking_spaces,
+    }
+
+    if created:
+        # Add the property if it's a new one
+        real_state_recommender.add_property(property_data)
+    else:
+        # Remove the old property if it's an update
+        real_state_recommender.remove_property(instance.id)
+        # Add the updated property
+        real_state_recommender.add_property(property_data)
 
 
 @receiver(post_delete, sender=RealState)
-def update_recommendation_on_delete(sender, instance, **kwargs):
-    real_state_recommender.load_properties()
+def handle_property_delete(sender, instance, **kwargs):
+    # Remove the property from the recommender
+    real_state_recommender.remove_property(instance.id)
