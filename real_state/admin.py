fbaf77d116d9683482f377bcd0d07be2a7ce6954
Omar <omar1752003@gmail.com>
2025-01-26 15:21:48 +0200

    models

--- real_state/admin.py
+++ real_state/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import RealState
+from .models import RealState, UserInteraction
 
 admin.site.register(RealState)
+admin.site.register(UserInteraction)
---
+++ real_state/models/__init__.py
@@ -0,0 +1,2 @@
+from .real_state import RealState
+from .user_interaction import UserInteraction
