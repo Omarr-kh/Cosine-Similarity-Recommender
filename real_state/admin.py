2e44166f0712dad7933fdcb06d33d681a50a6305
Omar <omar1752003@gmail.com>
2025-01-29 15:12:21 +0200

    added new models to admin panel

--- real_state/admin.py
+++ real_state/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import RealState, UserInteraction
+from .models import RealState, UserInteraction, Location, Feature
 
 admin.site.register(RealState)
 admin.site.register(UserInteraction)
+admin.site.register(Location)
+admin.site.register(Feature)
